e486fb90b53df95a68dae7b7c0ea6cdc4636f225
Ankan Bag <91788199+akbng@users.noreply.github.com>
2022-09-02 02:02:49 +0530

    add functionality of bottom navigation

--- components/BottomNav.jsx
+++ components/BottomNav.jsx
@@ -1,21 +1,48 @@
-import { RiHistoryLine, RiHomeLine, RiMenuLine, RiUser3Line } from "react-icons/ri";
+import {
+  RiHistoryLine,
+  RiHomeLine,
+  RiMenuLine,
+  RiUser3Line,
+} from "react-icons/ri";
 
-const BottomNav = ({ className }) => {
+const BottomNav = ({ className, active, setActive }) => {
+  const changeTab = (tab) => () => setActive(tab);
+
   return (
     <nav className={className}>
       <div className="w-full h-full text-2xl text-white flex justify-evenly items-center">
-        <div>
-          <RiHomeLine />
-        </div>
-        <div>
-          <RiUser3Line />
-        </div>
-        <div>
-          <RiHistoryLine />
-        </div>
-        <div>
-          <RiMenuLine />
-        </div>
+        <button
+          className={`w-8 h-full border-white ${
+            active === "home" && "border-b-2"
+          }`}
+          onClick={changeTab("home")}
+        >
+          <RiHomeLine className="mx-auto" />
+        </button>
+        <button
+          className={`w-8 h-full border-white ${
+            active === "friends" && "border-b-2"
+          }`}
+          onClick={changeTab("friends")}
+        >
+          <RiUser3Line className="mx-auto" />
+        </button>
+        <button
+          className={`w-8 h-full border-white  ${
+            active === "transactions" && "border-b-2"
+          }`}
+          onClick={changeTab("transactions")}
+        >
+          <RiHistoryLine className="mx-auto" />
+        </button>
+        <button
+          className={`w-8 h-full border-white  ${
+            active === "menu" && "border-b-2"
+          }`}
+          onClick={changeTab("menu")}
+        >
+          <RiMenuLine className="mx-auto" />
+        </button>
       </div>
     </nav>
   );
