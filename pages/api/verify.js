ed7665e0d0f6e7143c55b1a3a2774b250a1b14ca
Ankan Bag <91788199+akbng@users.noreply.github.com>
2022-09-02 22:01:14 +0530

    added verify api route to check for jwt integrity

---
+++ pages/api/verify.js
@@ -0,0 +1,29 @@
+import connectDB from "../../config/database";
+import { verifyJwt } from "../../lib/jwt";
+
+connectDB();
+
+export default async function (req, res) {
+  const { method } = req;
+
+  if (method === "GET") {
+    if (!req.body)
+      return res.status(400).json({
+        error: true,
+        reason: "There is no data/body in the request",
+      });
+
+    try {
+      const decodedPayload = await verifyJwt(req.query.tok);
+      return res.status(200).json({ error: false, data: decodedPayload });
+    } catch (error) {
+      return res
+        .status(401)
+        .json({ error: true, reason: error.reason || error.message });
+    }
+  }
+  return res.status(405).json({
+    error: true,
+    reason: `${method} request is not allowed for this route`,
+  });
+}
