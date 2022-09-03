661f07321e6747ad7185f241c8a1feae5f5ba2e4
Ankan Bag <91788199+akbng@users.noreply.github.com>
2022-09-03 22:09:39 +0530

    update profile tab to use controlled inputs

--- components/PhoneTab.jsx
+++ components/PhoneTab.jsx
@@ -2,10 +2,9 @@ import { useState } from "react";
 import { FaInfoCircle } from "react-icons/fa";
 import { RiLoader2Line, RiSendPlaneFill } from "react-icons/ri";
 
-const PhoneTab = () => {
+const PhoneTab = ({ values, handleChange }) => {
   const [loading, setLoading] = useState(false);
   const [otpSent, setOtpSent] = useState(false);
-  const [verified, setVerified] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -26,9 +25,12 @@ const PhoneTab = () => {
           title="Ten digits Phone Number"
           className="w-full py-2 pl-3 pr-8 border focus:outline-none rounded-md bg-purple-50 text-gray-800 shadow-xl"
           placeholder="Enter your Phone Number..."
+          value={values.phoneNumber}
+          onChange={handleChange("phoneNumber")}
+          disabled={otpSent}
           required
         />
-        {!otpSent ? (
+        {!otpSent && (
           <button
             type="submit"
             disabled={loading}
@@ -40,20 +42,23 @@ const PhoneTab = () => {
               <RiSendPlaneFill />
             )}
           </button>
-        ) : (
-          <div className="w-full my-6 relative">
-            <input
-              type="text"
-              className="w-full py-2 px-3 border focus:outline-none rounded-md bg-purple-50 text-gray-800 shadow-xl"
-              placeholder="Enter the Code sent to you..."
-            />
-            <h4 className="absolute mt-4 top-full text-xs text-purple-50">
-              <FaInfoCircle className="inline-block" /> Please enter 000000 as
-              the code
-            </h4>
-          </div>
         )}
       </form>
+      {otpSent && (
+        <div className="w-full my-6 relative">
+          <input
+            type="text"
+            className="w-full py-2 px-3 border focus:outline-none rounded-md bg-purple-50 text-gray-800 shadow-xl"
+            placeholder="Enter the Code sent to you..."
+            value={values.otp}
+            onChange={handleChange("otp")}
+          />
+          <h4 className="absolute mt-4 top-full text-xs text-purple-50">
+            <FaInfoCircle className="inline-block" /> Please enter 000000 as the
+            code
+          </h4>
+        </div>
+      )}
     </div>
   );
 };
