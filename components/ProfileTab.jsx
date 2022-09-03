986b1603c9b849cd3440efad67b195fcda96d347
Ankan Bag <91788199+akbng@users.noreply.github.com>
2022-09-03 22:08:42 +0530

    update profile tab to use control inputs from props

--- components/ProfileTab.jsx
+++ components/ProfileTab.jsx
@@ -1,28 +1,34 @@
 import { useState } from "react";
 import { FaRegEye, FaRegEyeSlash } from "react-icons/fa";
 
-const ProfileTab = () => {
+const ProfileTab = ({ values, handleChange }) => {
   const [showPass, setShowPass] = useState(false);
   const [showCoPass, setShowCoPass] = useState(false);
 
   return (
     <div>
-      <h1 className="text-2xl mb-6 text-purple-50">Complete your profile</h1>
+      <h1 className="text-2xl mb-6 text-purple-50">Fill Personal Info</h1>
       <input
         type="text"
         className="w-full my-2 py-2 px-3 border focus:outline-none rounded-md bg-purple-50 text-gray-800 shadow-xl"
         placeholder="Enter your First Name..."
+        value={values.firstName}
+        onChange={handleChange("firstName")}
       />
       <input
         type="text"
         className="w-full my-2 py-2 px-3 border focus:outline-none rounded-md bg-purple-50 text-gray-800 shadow-xl"
         placeholder="Enter your Last Name..."
+        value={values.lastName}
+        onChange={handleChange("lastName")}
       />
       <div className="relative w-full">
         <input
           type={showPass ? "text" : "password"}
           className="w-full my-2 py-2 pl-3 pr-8 text-lg border focus:outline-none rounded-md bg-purple-50 text-gray-800 shadow-xl"
           placeholder="Enter your password..."
+          value={values.password}
+          onChange={handleChange("password")}
         />
         <div
           onClick={() => setShowPass(!showPass)}
@@ -40,6 +46,8 @@ const ProfileTab = () => {
           type={showCoPass ? "text" : "password"}
           className="w-full my-2 py-2 pl-3 pr-8 text-lg border focus:outline-none rounded-md bg-purple-50 text-gray-800 shadow-xl"
           placeholder="Confirm your password..."
+          value={values.confirmPassword}
+          onChange={handleChange("confirmPassword")}
         />
         <div
           onClick={() => setShowCoPass(!showCoPass)}
