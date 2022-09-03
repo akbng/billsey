7e15c85488b2efb131c925102ebbf97df13fd4fa
Ankan Bag <91788199+akbng@users.noreply.github.com>
2022-09-04 01:37:00 +0530

    added utility function to set token to localstorage

--- utils/index.js
+++ utils/index.js
@@ -27,3 +27,18 @@ export const generateColors = () => {
     complimentary2: hsl(complimentaryHue2, saturation, luminosity),
   };
 };
+
+export const authenticate = (token) => {
+  if (typeof window !== "undefined")
+    return localStorage.setItem("token", JSON.stringify(token));
+  else return false;
+};
+
+export const isAuthenticated = () => {
+  if (typeof window === "undefined") return false;
+  const data = JSON.parse(localStorage.getItem("token"));
+  if (data && data.expiry * 1000 > Date.now()) {
+    console.log(data);
+    return data;
+  } else return false;
+};
