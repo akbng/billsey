989542b1c102ab8997d6993bcc7cb82f5e14d74d
Ankan Bag <91788199+akbng@users.noreply.github.com>
2022-09-04 13:12:12 +0530

    add group model

---
+++ models/Group.js
@@ -0,0 +1,53 @@
+import { Schema, models, model } from "mongoose";
+const { ObjectId } = Schema.Types;
+
+const groupSchema = new Schema(
+  {
+    name: {
+      type: String,
+      maxLength: 30,
+      required: true,
+      trime: true,
+    },
+    description: {
+      type: String,
+      maxLength: 128,
+      trim: true,
+    },
+    tags: [{ type: String }],
+    creator: {
+      type: ObjectId,
+      ref: "User",
+      required: true,
+    },
+    members: [
+      {
+        type: ObjectId,
+        ref: "User",
+      },
+    ],
+    transactions: [
+      {
+        type: ObjectId,
+        ref: "Transaction",
+      },
+    ],
+    bills: [
+      {
+        name: {
+          type: String,
+          required: true,
+        },
+        totalAmount: {
+          type: Number,
+          required: true,
+        },
+        members: [{ type: ObjectId, ref: User }],
+        splitAmount: Number,
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+export default models.Group || model("Group", groupSchema);
