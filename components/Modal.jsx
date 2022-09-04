754a3ba7e672397956975c694d01f623e17dc089
Ankan Bag <91788199+akbng@users.noreply.github.com>
2022-09-04 20:15:21 +0530

    add customisation of panel colors

--- components/Modal.jsx
+++ components/Modal.jsx
@@ -1,7 +1,14 @@
 import { Dialog, Transition } from "@headlessui/react";
 import { Fragment } from "react";
 
-const Modal = ({ isOpen, setIsOpen, title, children }) => {
+const Modal = ({
+  isOpen,
+  setIsOpen,
+  title,
+  children,
+  bgPanel = "bg-white",
+  textColor = "text-gray-900",
+}) => {
   const closeModal = () => setIsOpen(false);
 
   return (
@@ -30,10 +37,12 @@ const Modal = ({ isOpen, setIsOpen, title, children }) => {
               leaveFrom="opacity-100 scale-100"
               leaveTo="opacity-0 scale-95"
             >
-              <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-lg bg-white p-6 text-left align-middle shadow-xl transition-all">
+              <Dialog.Panel
+                className={`w-full max-w-md transform overflow-hidden rounded-lg p-6 text-left align-middle shadow-xl transition-all ${bgPanel}`}
+              >
                 <Dialog.Title
                   as="h3"
-                  className="text-lg font-medium leading-6 text-gray-900"
+                  className={`text-lg font-medium leading-6 ${textColor}`}
                 >
                   {title}
                 </Dialog.Title>
