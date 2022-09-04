0c025d03fa5735a7f3020ec6e3f1cfa9ca1fa263
Ankan Bag <91788199+akbng@users.noreply.github.com>
2022-09-05 00:42:19 +0530

    add user helper functions

---
+++ helper/user.js
@@ -0,0 +1,28 @@
+const baseUrl = "http://localhost:3000/api";
+
+export const getUsers = (token) =>
+  fetch(`${baseUrl}/users`, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  }).then((res) => res.json());
+
+export const getFriends = (token) =>
+  fetch(`${baseUrl}/friends`, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  }).then((res) => res.json());
+
+export const addFriend = (friendId, token) =>
+  fetch(`${baseUrl}/friend/add`, {
+    method: "PUT",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify({ friendId }),
+  });
