4a2a6d044e371a4eb71ca7dc33d990e4550721c9
Ankan Bag <91788199+akbng@users.noreply.github.com>
2022-09-04 20:15:04 +0530

    render actual group instead of dummy data

--- components/GroupCard.jsx
+++ components/GroupCard.jsx
@@ -1,33 +1,29 @@
 import { FaPlus, FaTag } from "react-icons/fa";
 import { HiOutlineUserGroup } from "react-icons/hi";
 
-const GroupCard = ({ className }) => {
+const GroupCard = ({ className, data }) => {
   return (
     <div className={className}>
       <div className="text-xl text-gray-200">
-        <HiOutlineUserGroup className="inline" /> GROUP_NAME
+        <HiOutlineUserGroup className="inline" /> {data.name}
       </div>
       <div className="w-full mt-0 mb-3 flex justify-start items-center flex-wrap">
-        {Array(4)
-          .fill("")
-          .map((_, i) => (
-            <div
-              className="w-fit mt-1 px-3 py-1 mx-1 first:ml-0 text-[0.5rem] text-gray-700 bg-white rounded"
-              key={i}
-            >
-              <FaTag className="inline-block" /> Tag
-            </div>
-          ))}
-        <div className="w-5 h-5 mt-1 ml-1 group">
+        {data.tags.map((tag, i) => (
+          <div
+            className="w-fit mt-1 px-2 py-1 mr-1 text-[0.5rem] text-gray-700 bg-white rounded"
+            key={i}
+          >
+            <FaTag className="inline-block" /> {tag}
+          </div>
+        ))}
+        <div className="w-5 h-5 mt-1 group">
           <button className="px-2 py-[0.385rem] transition-all hidden group-hover:block text-[0.5rem] text-gray-700 bg-white rounded">
             <FaPlus />
           </button>
         </div>
       </div>
       <div className="my-2 w-full max-h-16 overflow-hidden text-sm text-gray-300">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium,
-        quidem. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem,
-        odit?
+        {data.description}
       </div>
       <button className="absolute bottom-6 z-10 uppercase text-white rounded-md px-6 py-2 font-medium transition-colors duration-100 bg-purple-800 hover:bg-purple-600">
         Split Bill
--- components/Groups.jsx
+++ components/Groups.jsx
@@ -1,20 +1,63 @@
+import { useState, useEffect } from "react";
 import { FaPlus } from "react-icons/fa";
+import { getGroupsOfMember } from "../helper/group";
+import { isAuthenticated } from "../utils";
+
 import GroupCard from "./GroupCard";
+import Modal from "./Modal";
 
 const Groups = ({ className }) => {
+  const [groups, setGroups] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [isCreateOpen, setIsCreateOpen] = useState(false);
+
+  useEffect(() => {
+    const init = async () => {
+      setLoading(true);
+      const { user, token } = isAuthenticated();
+      try {
+        const result = await getGroupsOfMember(user._id, token);
+        if (result.error) throw Error(result.reason);
+        setGroups(result.data);
+      } catch (err) {
+        console.error(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+    init();
+  }, []);
+
   return (
     <main className={className}>
-      {Array(8)
-        .fill("")
-        .map((_, i) => {
+      {loading ? (
+        Array(3)
+          .fill("")
+          .map((_, i) => (
+            <div
+              className="w-11/12 h-60 my-8 first:mt-4 p-6 rounded-lg mx-auto max-w-xs bg-gray-900 animate-pulse"
+              key={i}
+            />
+          ))
+      ) : groups.length > 0 ? (
+        groups.map((group, i) => {
           return (
             <GroupCard
               key={i}
+              data={group}
               className="w-11/12 h-60 my-8 first:mt-4 p-6 relative rounded-lg mx-auto max-w-xs bg-black bg-opacity-50 shadow-lg"
             />
           );
-        })}
-      <button className="fixed bottom-14 right-4 z-20 text-white bg-gradient-to-l from-indigo-900 to-purple-900 py-2 px-4 rounded">
+        })
+      ) : (
+        <div className="absolute inset-0 p-8 flex justify-center items-center text-center text-gray-100 text-2xl">
+          PLEASE ADD NEW GROUP!
+        </div>
+      )}
+      <button
+        className="fixed bottom-14 right-4 z-10 text-white bg-gradient-to-l from-indigo-900 to-purple-900 py-2 px-4 rounded"
+        onClick={() => setIsCreateOpen(true)}
+      >
         <FaPlus className="inline mr-1" /> New Group
       </button>
     </main>
